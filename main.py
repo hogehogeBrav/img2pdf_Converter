999edeb9fa641075d7abf8f8d4d9c8a695102492
hogehogeBrav <zmzmakoto@icloud.com>
2022-11-02 00:49:04 +0900

    [update] .gitignore

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from natsort import natsorted
 from PIL import Image
 
 if __name__ == '__main__':
-    pdf_FileName = "./output.pdf"
-    png_Folder = "./png"
-    extension  = ".png"
-    listfolder = os.listdir(png_Folder)
-    sortfolder = natsorted(listfolder)
+  pdf_FileName = "./output.pdf"
+  png_Folder = "./png"
+  extension  = ".png"
+  listfolder = natsorted(os.listdir(png_Folder))
 
+  if not listfolder:
+    print("No files found in the folder")
+    exit()
+  else:
     with open(pdf_FileName,"wb") as f:
-
-        f.write(img2pdf.convert([Image.open(png_Folder+j).filename for j in sortfolder if j.endswith(extension)]))
+      f.write(img2pdf.convert([Image.open(png_Folder+j).filename for j in listfolder if j.endswith(extension)]))
