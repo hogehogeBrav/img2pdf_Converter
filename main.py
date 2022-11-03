fc36ae77aed659e87a2a6beb30198752b1c63520
hogehogeBrav <zmzmakoto@icloud.com>
2022-11-03 10:18:45 +0900

    [update] folder check, file ext check

--- main.py
+++ main.py
@@ -3,15 +3,36 @@ import img2pdf
 from natsort import natsorted
 from PIL import Image
 
-if __name__ == '__main__':
-  pdf_FileName = "./output.pdf"
-  png_Folder = "./png"
-  extension  = ".png"
-  listfolder = natsorted(os.listdir(png_Folder))
+# フォルダ内拡張子チェック
+def extCheck(list, ext):
+  for filename in list:
+    if not filename.endswith(ext):
+      return False
+  return True
 
-  if not listfolder:
-    print("No files found in the folder")
+if __name__ == '__main__':
+  # 許可するファイル拡張子
+  extension  = ".png", ".jpg", ".jpeg" # tuple 拡張子
+
+  # PDFファイル名入力
+  pdf_FileName = "./" + input('Please type generate PDF filename:') + ".pdf"
+
+  # 画像格納フォルダ
+  png_Folder = "./png"
+  listfolder = natsorted(os.listdir(png_Folder)) # 数字順にソート
+
+  print(listfolder)
+
+  if not listfolder: # フォルダ内にファイルがない場合
+    print("!! No files found in the folder !!")
+    exit()
+  elif not extCheck(listfolder, extension): # フォルダ内に許可する拡張子のファイルがない場合
+    print("!! Only image files should be stored in the folder (Example...[.png, .jpg, .jpeg]) !!")
     exit()
   else:
     with open(pdf_FileName,"wb") as f:
-      f.write(img2pdf.convert([Image.open(png_Folder+j).filename for j in listfolder if j.endswith(extension)]))
+      f.write(img2pdf.convert([Image.open(png_Folder + "/" + j).filename for j in listfolder if j.endswith(extension)]))
+
+      print("-----------------")
+      print("|    Success!   |")
+      print("-----------------")
